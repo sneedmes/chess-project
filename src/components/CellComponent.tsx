5bd5982c9db3182d03238f724a4aa52038759c0a
sneedmes <dallaqyan0610@gmail.com>
2025-01-20 21:39:04 +0400

    feat: created figures and set them on board

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -8,7 +8,7 @@ interface CellProps {
 export const CellComponent = ({cell}: CellProps) => {
     return (
         <div className={["cell", cell.color].join(" ")}>
-
+            {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
         </div>
     );
 };
