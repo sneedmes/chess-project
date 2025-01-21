1cfd824ce1e26b6f6bd2324a1ac70bf1f2a33071
sneedmes <dallaqyan0610@gmail.com>
2025-01-21 23:57:42 +0400

    feat: wrote logic for replacing figures

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,11 +3,18 @@ import {Cell} from "../models/Cell";
 
 interface CellProps {
     cell: Cell
+    selected: boolean;
+    click: (cell: Cell)=>void
 }
 
-export const CellComponent = ({cell}: CellProps) => {
+export const CellComponent = ({cell, selected, click}: CellProps) => {
     return (
-        <div className={["cell", cell.color].join(" ")}>
+        <div
+            className={["cell", cell.color, selected ? "selected" : ""].join(" ")}
+            onClick={()=>click(cell)}
+            style={{background: cell.available && cell.figure ? "lightpink" : ""}}
+        >
+            {cell.available && !cell.figure && <div className={"available"}/>}
             {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
         </div>
     );
