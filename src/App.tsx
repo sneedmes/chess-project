c57a390a1cccbd9430eebc8f7a3710a214075c5a
sneedmes <dallaqyan0610@gmail.com>
2025-01-23 23:57:29 +0400

    feat: described knight's and pawn's movements

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import {BoardComponent} from "./components/BoardComponent";
 import {Board} from "./models/Board";
+import {Player} from "./models/Player";
+import {Colors} from "./models/Colors";
 
 function App() {
   const [board, setBoard] = useState(new Board())
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+
 
   useEffect(()=>{
       restart()
